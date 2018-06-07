b5d15a0ea1da1dc45d098546a1be20e2e21cab87
Pavel Vaysberg <pavel@draft.com>
2018-06-07 20:52:53 +0000

    different firewall rules for each node tag

--- main.tf
+++ main.tf
@@ -103,8 +103,8 @@ resource "google_compute_route" "nat-gateway" {
 }
 
 resource "google_compute_firewall" "nat-gateway" {
-  count   = "${var.module_enabled ? 1 : 0}"
-  name    = "${local.zonal_tag}"
+  count   = "${length(var.tags)}"
+  name    = "${var.name}nat-${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}-${count.index}"
   network = "${var.network}"
   project = "${var.project}"
 
@@ -112,8 +112,8 @@ resource "google_compute_firewall" "nat-gateway" {
     protocol = "all"
   }
 
-  source_tags = ["${compact(concat(list("${local.regional_tag}", "${local.zonal_tag}"), var.tags))}"]
-  target_tags = ["${compact(concat(local.instance_tags, var.tags))}"]
+  source_tags = ["${compact(concat(list("${var.name}nat-${var.region}", "${var.name}nat-${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}"), "${list(element(var.tags, count.index))}))}"]
+  target_tags = ["${compact(concat(list("${var.name}nat-${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}"), "${list(element(var.tags, count.index))}"))}"]
 }
 
 resource "google_compute_address" "default" {
